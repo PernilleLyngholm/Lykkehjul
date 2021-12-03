dca5a6887c640e9c6fc4a844ec686ae97580dfa0
Pernille <s205361@student.dtu.dk>
2021-12-03 20:48:14 +0100

    resource strings updated

--- app/src/main/java/com/example/lykkehjul/screens/gameResult/GameResultFragment.kt
+++ app/src/main/java/com/example/lykkehjul/screens/gameResult/GameResultFragment.kt
@@ -3,12 +3,20 @@ package com.example.lykkehjul.screens.gameResult
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.lykkehjul.R
+import com.example.lykkehjul.screens.category.CategoryFragmentDirections
+import kotlinx.android.synthetic.main.fragment_result_game.*
 
 class GameResultFragment: Fragment(R.layout.fragment_result_game) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        tryAgainButton.setOnClickListener {
+            val action = GameResultFragmentDirections.actionGameResultFragmentToGameFragment()
+            findNavController().navigate(action)
+        }
 
     }
 }
